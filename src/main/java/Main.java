9d243673089aec9b8d98ac0beedd9e0ef395a8c0
Rafal Szczotka <rafalszczotka@wp.pl>
2019-01-13 13:15:15 +0100

    next changes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,32 +1,9 @@
-import java.util.List;
+
 
 public class Main {
 
-    private List<User> listOfUsers;
-
-    int numberOfRates;
-
-
-
-    public boolean checkIfUserExist(String email) {
-        if (listOfUsers != null) {
-
-            for (User u : listOfUsers) {
-                if (u.getEmail().contains(email)) {
-                    Loger.printInfo("This email " + email + " is busy.");
-                    return true;
-                }
-            }
-        }return false;
-    }
-
-
-
 
     public static void main(String[] args) {
-
-
-
 
 
 
--- src/main/java/Payment.java
+++ src/main/java/Payment.java
@@ -3,14 +3,31 @@ import java.util.List;
 
 public class Payment {
 
-    Date dueDate;
-    List<Installment> listOfInstallments;
+    private Date dueDate;
+    private List<Installment> listOfInstallments;
+    private Installment installment;
+    private double expectedTotalAmount;
+    private double actualTotalAmount;
 
     public Payment(Date dueDate, List<Installment> listOfInstallments) {
         this.dueDate = dueDate;
         this.listOfInstallments = listOfInstallments;
     }
 
+    //should has this method some parameters?
+    public double getExpectedTotalAmount() {
+      for (Installment e : listOfInstallments) {
+          expectedTotalAmount += e.getExpectedAmount();
+      }
+      return expectedTotalAmount;
+    }
+
+    public double getActualTotalAmount() {
+        for (Installment e : listOfInstallments) {
+            actualTotalAmount += e.getActualAmount();
+        }
+        return actualTotalAmount;
+    }
 
 
 }
