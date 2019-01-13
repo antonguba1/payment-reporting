4e4f82036d4728b4f868216ae1dd0514cdf13280
Rafal Szczotka <rafalszczotka@wp.pl>
2019-01-13 13:18:22 +0100

    new vision

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,45 +1,8 @@
-<<<<<<< HEAD
-=======
-import java.util.List;
-import java.util.Scanner;
-
-public class Main {
-
-    public static int numberOfUsers=0;
-    private List<User> listOfUsers;
-
-
-    public void addUser(String name, String email) {
-        User user = new User(name, email);
-        listOfUsers.add(user);
-        numberOfUsers++;
-    }
-
-    public boolean checkIfUserExist(String email) {
-        if (listOfUsers != null) {
-
-            for (User u : listOfUsers) {
-                if (u.getEmail().contains(email)) {
-                    Loger.printInfo("This email " + email + " is busy.");
-                    return true;
-                }
-            }
-        }return false;
-    }
->>>>>>> 1f556428250419addefbb82e149787f760301508
-
-
 public class Main {
 
 
     public static void main(String[] args) {
 
-<<<<<<< HEAD
-=======
-        Loger.printInfo("Write your email.");
-        Scanner input = new Scanner(System.in);
-
->>>>>>> 1f556428250419addefbb82e149787f760301508
 
 
     }
--- src/main/java/Payment.java
+++ src/main/java/Payment.java
@@ -4,7 +4,6 @@ import java.util.List;
 public class Payment {
 
     private Date dueDate;
-<<<<<<< HEAD
     private List<Installment> listOfInstallments;
     private Installment installment;
     private double expectedTotalAmount;
@@ -14,43 +13,21 @@ public class Payment {
         this.dueDate = dueDate;
         this.listOfInstallments = listOfInstallments;
     }
-=======
-    private Date actualDate;
-    private double expectedAmount;
-    private double actualAmount;
-    private String status;
->>>>>>> 1f556428250419addefbb82e149787f760301508
 
     //should has this method some parameters?
     public double getExpectedTotalAmount() {
-      for (Installment e : listOfInstallments) {
-          expectedTotalAmount += e.getExpectedAmount();
-      }
-      return expectedTotalAmount;
+        for (Installment e : listOfInstallments) {
+            expectedTotalAmount += e.getExpectedAmount();
+        }
+        return expectedTotalAmount;
     }
 
-<<<<<<< HEAD
     public double getActualTotalAmount() {
         for (Installment e : listOfInstallments) {
             actualTotalAmount += e.getActualAmount();
         }
         return actualTotalAmount;
-=======
-    public Payment(int expectedAmount, int numberOfPayments) {
-        this.expectedAmount = expectedAmount;
-    }
-
-    public void setDueDate(Date dueDate) {
-        this.dueDate = dueDate;
->>>>>>> 1f556428250419addefbb82e149787f760301508
     }
 
 
-    public String checkPaymentStatus() {
-        if (expectedAmount > actualAmount && (actualDate.after(dueDate))) {
-            return status = "LATE";
-        } else if (expectedAmount <= actualAmount && ((actualDate.before(dueDate)) || ((actualDate.equals(dueDate))) {
-            return status = "ON TIME";
-        }
-    }
 }
